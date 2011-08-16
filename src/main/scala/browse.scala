f27c4d2d93a89c9b9db3d90f35df0890cbfd52f3
softprops <d.tangren@gmail.com>
2011-08-16 19:46:33 -0400

    basic upload and persistance

--- src/main/scala/browse.scala
+++ src/main/scala/browse.scala
@@ -4,6 +4,23 @@ import unfiltered.request._
 import unfiltered.response._
 
 import org.clapper.avsl.Logger
+import java.io.{ByteArrayOutputStream, InputStream}
+
+
+object Pipe {
+  def apply(out: ByteArrayOutputStream)(in: InputStream) = {
+    @annotation.tailrec
+    def consume(buffer: Array[Byte]): ByteArrayOutputStream  = in.read(buffer, 0, buffer.length) match {
+      case -1 =>
+        out.flush()
+        out
+      case read =>
+         out.write(buffer, 0, read)
+         consume(buffer)
+    }
+    consume(new Array[Byte](1024*16))
+  }
+}
 
 /** Browser front end, serves the page and handles multipart uploads */
 object Browse extends unfiltered.filter.Plan {
@@ -12,8 +29,38 @@ object Browse extends unfiltered.filter.Plan {
   def intent = {
     case GET(Path("/")) =>
       logger.debug("GET /")
-      Ok ~> view(<p> bitshow </p> <img src="/test"/>)
+      Ok ~> view(<p><form enctype="multipart/form-data" action="/up" method="POST">
+        <input type="file" name="f" />
+        <input type="submit" value="post"/>
+       </form></p>)
+       
+    case POST(Path("/up") & MultiPart(req)) =>
+       MultiPartParams.Streamed(req).files("f") match {
+         case Seq(file, _*) =>
+            val out = new ByteArrayOutputStream()
+            file.stream(Pipe(out))
+            val bytes = out.toByteArray
+            val id = DefaultStore.put(Item(file.contentType, bytes))
+            Redirect("/previews/%s" format id)
+         case _ => BadRequest ~> ResponseString("file f required")
+       }
+       
+     case GET(Path(Seg("previews" :: id :: Nil))) =>
+        DefaultStore.get(id) match {
+          case Some(item) => view(<p><img src={"/files/%s" format id}/></p>)
+          case _ => NotFound
+        }
+    
+     case GET(Path(Seg("files" :: id :: Nil))) =>
+        DefaultStore.get(id) match {
+          case Some(item) =>
+            ContentType(item.contentType) ~> ContentLength(item.bytes.size.toString) ~>
+              ResponseBytes(item.bytes)
+          case _ => NotFound
+        }
+    
   }
+
   def view(body: scala.xml.NodeSeq) = {
     Html(
      <html>
