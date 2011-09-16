41c85607d8341af1c1f3ca24409c709706b15bfd
Nikolai Avteniev <nikolai.avteniev+github@gmail.com>
2011-09-15 23:26:06 -0400

    added a RESTful API method for creating image conversions

--- src/main/resources/www/js/bitshow.js
+++ src/main/resources/www/js/bitshow.js
@@ -1,19 +1,19 @@
-(function($){
-  $(function(){
-    window.bitshow = (function() {
-      var api = 'http://' + window.location.host;
-      return {
-        converters: function(fn) {
-          $.getJSON(api + '/converters', fn);
-        },
-        convert: function(converter, id, fn) {
-          $.post(api + '/convert/' + id + "/" + converter, fn);
-        },
-        images: function(fn) {
-          $.getJSON(api + "/images", fn);
-        }
+(function($) {
+    $(function() {
+        window.bitshow = (function() {
+            var api = 'http://' + window.location.host;
+            return {
+                converters: function(fn) {
+                    $.getJSON(api + '/converters', fn);
+                },
+                convert: function(converter, id, fn) {
+                    $.post(api + '/conversions', JSON.stringify({ id: id, converter: converter}), fn);
+                },
+                images: function(fn) {
+                    $.getJSON(api + "/images", fn);
+                }
 
-      };
+            };
+        });
     });
-  });
 })(jQuery);
--- src/main/scala/api.scala
+++ src/main/scala/api.scala
@@ -6,26 +6,33 @@ import unfiltered.response._
 import net.liftweb.json.JsonDSL._
 
 import org.clapper.avsl.Logger
+import java.net.URLDecoder.decode
+import net.liftweb.json.JsonAST.{JString, JField, JObject}
 
-/** This filter should handle conversions requested
+/**This filter should handle conversions requested
  * by the client page, serve listings of available
  * images and converters, and serve the images themselves */
 object API extends unfiltered.filter.Plan {
   val logger = Logger(getClass)
-  
+
   def intent = {
-    case POST(Path(Seg("convert" :: id :: converter :: Nil))) =>
-      val c = java.net.URLDecoder.decode(converter, "UTF-8")
-      val optI = Converters(c).flatMap(DefaultStore.get(id).map)
-      optI.map(DefaultStore.put).map(id => Json("id" -> id)).getOrElse(NotFound)
-    
+    case req@POST(Path("/conversions")) =>
+      JsonBody(req) match {
+        case Some(JObject(JField("id", JString(id)) :: JField("converter", JString(converter)) :: Nil)) => {
+          val converterOpt: Option[Converter] = Converters(decode(converter, "UTF-8"))
+          val optionItem: Option[Item] = DefaultStore.get(id)
+          converterOpt.flatMap(optionItem.map).map(DefaultStore.put).map(id => Json("id" -> id)).getOrElse(NotFound)
+        }
+        case _ => BadRequest
+      }
+
     case GET(Path("/converters")) => Json(Converters.names)
-    
+
     case GET(Path("/images")) => Json(DefaultStore.list)
-    
+
     case GET(Path(Seg("images" :: id :: Nil))) =>
       DefaultStore.get(id).map(i => ContentType(i.contentType) ~> ResponseBytes(i.bytes)).getOrElse(NotFound)
-      
+
     case GET(Path("/test")) =>
       val bytes = org.apache.commons.io.IOUtils.toByteArray(
         getClass.getResource("/www/img/ny-scala.png").openStream
